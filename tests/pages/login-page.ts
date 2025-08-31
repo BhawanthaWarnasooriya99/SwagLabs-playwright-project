9561a062469c4b1af20c57c152b6a4e3bcad2197
BhawanthaWarnasooriya99 <bhawantha1002@gmail.com>
2025-09-01 01:58:53 +0530

    modified login and invetory files. added utils files

--- tests/pages/login-page.ts
+++ tests/pages/login-page.ts
@@ -1,4 +1,5 @@
-import {type Locator, type Page} from "@playwright/test";
+import {expect, type Locator, type Page} from "@playwright/test";
+import messages from "../utils/messages";
 
 export class LoginPage {
 
@@ -7,6 +8,7 @@ export class LoginPage {
     readonly loginButton: Locator;
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
+    readonly errorMessage: Locator;
 
 
     //contructors
@@ -15,13 +17,43 @@ export class LoginPage {
         this.loginButton = page.getByRole('button', {name: 'Login'});
         this.usernameInput = page.getByRole('textbox', {name: 'Username'});
         this.passwordInput = page.getByRole('textbox', {name: 'Password'});
+        this.errorMessage = page.locator("//div[@class='error-message-container error']");
     }
 
     //methods
+    async doLogin(username: string, password: string){
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+
+    async enterUsername(username: string) {
+        await this.usernameInput.fill(username);
+    }
+
+    async enterPassword(password: string) {
+        await this.passwordInput.fill(password);
+    }   
+
     async clickLoginButton() {
         await this.loginButton.click();
     }
 
+    async checkInvalidCredentials() {
+        await expect(this.errorMessage).toHaveText(messages.login.invalidUsernameAndPassword);
+    }
+
+    async checkedLockedOutUser() {
+        await expect(this.errorMessage).toHaveText(messages.login.lockedOutUser);
+    }
+
+    async checkedUsernameRequired() {
+        await expect(this.errorMessage).toHaveText(messages.login.usernameRequired);
+    }
+
+    async checkedPasswordRequired() {
+        await expect(this.errorMessage).toHaveText(messages.login.passwordRequired);
+    }
 
 }
 
--- tests/specs/inventory.spec.ts
+++ tests/specs/inventory.spec.ts
@@ -1,17 +1,18 @@
 import {test, expect} from '@playwright/test';
 import { InventoryPage } from '../pages/inventory-page';
-import { count } from 'console';
+import LoginPage from '../pages/login-page';
 
+let loginPage: LoginPage;
 let inventoryPage: InventoryPage;
 
 test.beforeEach(async ({ page }, testInfo) => {
-    console.log(`Running ${testInfo.title}`);
-    await page.goto('https://www.saucedemo.com/'); 
-    await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
-    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
-    await page.getByRole('button', { name: 'Login' }).click();
-
+    loginPage = new LoginPage(page);
     inventoryPage = new InventoryPage(page);
+
+    console.log(`Running ${testInfo.title}`);
+    await page.goto('https://www.saucedemo.com/');
+    await loginPage.doLogin('standard_user', 'secret_sauce'); 
+    
 });
 
 
